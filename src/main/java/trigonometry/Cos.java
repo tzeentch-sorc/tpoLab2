612e5d16d3d779e7ac77e591ab240edea96fcc1c
tzeentch-sorc <T.T585@yandex.ru>
2020-10-12 16:27:21 +0300

    Fix cos sign

--- src/main/java/trigonometry/Cos.java
+++ src/main/java/trigonometry/Cos.java
@@ -11,16 +11,18 @@ public class Cos implements Calculator {
 
     @Override
     public double calcValue(double x, double p) throws IllegalArgumentException {
-        double realX = calcX(x);
-        int sign = Math.abs(realX) < Math.PI/2 ? 1: -1;
+        double sign = calcSign(x);
+        //if(x == 10.1) System.out.println(Math.sqrt(1 - Math.pow(sin.calcValue(x, p*p), 2)) * sign);
         return Math.sqrt(1 - Math.pow(sin.calcValue(x, p*p), 2)) * sign;
     }
 
-    private static double calcX(double x) {
-        Double k = 0d;
-        if (Math.abs(x) > 2 * Math.PI)
-            k = x / (2 * Math.PI);
-        return x - 2 * k.intValue() * Math.PI;
+    private static double calcSign(double x) {
+        Double k;
+        if (Math.abs(x) > Math.PI/2) {
+            k = x / (Math.PI/2);
+            return Math.abs(k.intValue()) % 4 == 0 || Math.abs(k.intValue()) % 4 == 3 ? 1 : -1;
+        }
+        else return 1;
     }
 
     @Override
